7751e1b12472598f67f942a421edb4f535863f2a
Xavier Sala <utrescu@gmail.com>
2015-10-30 18:21:33 +0100

    Primer commit

---
+++ desktop/src/net/xaviersala/martina/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.xaviersala.martina.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.xaviersala.martina.MartinaApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MartinaApp(), config);
+	}
+}
